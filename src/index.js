54fb5624fb3233ccd117757d9706e355dda545c7
grim1998 <gmirmaksim8@gmail.com>
2023-01-25 17:53:41 +0300

    add:morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,27 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let resp = '';
+
+    for (let pos = 0; pos < expr.length; pos += 10) {
+        resp += getCharFromCode(expr.slice(pos, pos + 10));
+    }
+
+    return resp;
+}
+
+function getCharFromCode(code) {
+    if (code === '**********') return ' ';
+
+    let sumString = '';
+    for (let i = 0; i < 10; i += 2) {
+        const symbol = code.slice(i, i + 2);
+        if (symbol === '00') continue;
+        if (symbol === '10') sumString += '.';
+        if (symbol === '11') sumString += '-';
+    }
+    return MORSE_TABLE[sumString];
+
 }
 
 module.exports = {
